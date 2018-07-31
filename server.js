e741b36a0deff6063d2aa239336e8cb553d7a796
Kate Gibbs <kategibbs@Kates-MacBook-Pro-2.local>
2018-07-30 21:05:58 -0400

    functioning server.js

---
+++ server.js
@@ -0,0 +1,68 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+var bodyParser = require("body-parser");
+var path = require("path");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+// Restaurant (DATA)
+// =============================================================
+var tables = [];
+
+var waitList = [];
+
+// Routes
+// =============================================================
+
+// Basic route that sends the user first to the AJAX Page
+app.get("/", function(req, res) {
+  res.sendFile(path.join(__dirname, "home.html"));
+});
+
+app.get("/reserve", function(req, res) {
+  res.sendFile(path.join(__dirname, "reserve.html"));
+});
+
+app.get("/tables", function(req, res) {
+    res.sendFile(path.join(__dirname, "tables.html"));
+  });
+
+app.get("/api/tables", function(req, res) {
+  return res.json(tables);
+});
+
+app.get("/api/waitlist", function(req, res) {
+    return res.json(waitList);
+  });
+
+app.post("/api/tables", function(req, res) {
+
+  var newtable = req.body;
+  if (tables.length < 5) {
+      tables.push(newtable);
+  } else {
+      waitList.push(newtable);
+  }
+
+  console.log(newtable);
+  res.json(newtable);
+});
+
+app.post("/api/clear", function(req, res){
+    waitList = [];
+    tables = [];
+})
+
+// Starts the server to begin listening
+// =============================================================
+app.listen(PORT, function() {
+  console.log("App listening on PORT " + PORT);
+});
